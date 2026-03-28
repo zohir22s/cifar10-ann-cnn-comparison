fcc3ac845d23297f6a8526b1979facc5bc449a93
zohir22s <zohir22s@users.noreply.github.com>
2026-03-28 05:15:10 +0100

    add more layers to ann_model.py

--- models/ann_model.py
+++ models/ann_model.py
@@ -7,13 +7,12 @@ class ANN(nn.Module):
         super().__init__()
         # Define a simple feedforward neural network (ANN) for CIFAR-10 classification
         self.network = nn.Sequential(
-            # Input: 3x32x32 (CIFAR-10 images)
             nn.Flatten(),
-            nn.Linear(32*32*3, 512),
-            # Hidden layer 1
+            nn.Linear(32*32*3, 1024),
+            nn.ReLU(),
+            nn.Linear(1024, 512),
             nn.ReLU(),
             nn.Linear(512, 256),
-            # Hidden layer 2
             nn.ReLU(),
             nn.Linear(256, 10)
         )
